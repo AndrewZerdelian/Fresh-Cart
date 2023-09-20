4a4fa1190419c352991e731ffa6dbb8443b2d9a8
Andrew zerdelian <Andrew.h.zerdelian@gmail.com>
2023-09-21 01:18:01 +0300

    userToken

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -6,21 +6,16 @@ import Footer from "../Footer/Footer";
 import { Outlet } from "react-router-dom";
 import { userContext } from "../../Context/UserContext";
 
-
 export default function Layout() {
+  let { setUserToken } = useContext(userContext);
 
-   
-  
-   let {setUserToken} = useContext(userContext)
-
-  useEffect(()=> {
-    if (localStorage.getItem('userToken') !==null) {
-      setUserToken(localStorage.getItem('userToken'))
+  useEffect(() => {
+    if (localStorage.getItem("userToken") !== null) {
+      setUserToken(localStorage.getItem("userToken"));
     } else {
-      
     }
-  },[])
- 
+  }, []);
+
   return (
     <div>
       <NavBar />

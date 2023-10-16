d4de6ae0ac5f6295dd6773e87761ec17f17fec05
Andrew zerdelian <Andrew.h.zerdelian@gmail.com>
2023-10-16 19:37:39 +0300

    ok

--- src/Context/ForgotPasswordContext.js
+++ src/Context/ForgotPasswordContext.js
@@ -3,20 +3,38 @@ import React, { createContext } from "react";
 
 export const ForgetPassword = createContext();
 
-export default function ForgotPasswordContextProvider({children}) {
+export default function ForgotPasswordContextProvider({ children }) {
   async function POSTForgotPassword(email) {
     try {
       const response = await axios.post(
-        `https://ecommerce.routemisr.com/api/v1/auth/forgotPasswords`,email);
+        `https://ecommerce.routemisr.com/api/v1/auth/forgotPasswords`,
+        email
+      );
       //console.log("forget password was activated");
       return response;
     } catch (error) {
       console.log(error);
     }
   }
+  ///////////////////////POST Verify Reset Code////////////////////16/10/2023
 
+  async function POSTVerifyResetCode(resetCode) {
+    try {
+      const { data } = await axios.post(
+        `https://ecommerce.routemisr.com/api/v1/auth/verifyResetCode`,
+        {
+          resetCode,
+        }
+      );
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
-    <ForgetPassword.Provider value={{ POSTForgotPassword }}>
+    <ForgetPassword.Provider
+      value={{ POSTForgotPassword, POSTVerifyResetCode }}
+    >
       {children}
     </ForgetPassword.Provider>
   );

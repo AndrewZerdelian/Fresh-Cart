546ad04711b6d1d4a7cfe67f65cb1852cf9188f8
Andrew zerdelian <Andrew.h.zerdelian@gmail.com>
2023-10-24 20:07:34 +0300

    done

--- src/Components/MainSlider/MainSlider.jsx
+++ src/Components/MainSlider/MainSlider.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import Slider1 from "../../Assets/slider-image-1.jpeg";
 import Slider2 from "../../Assets/slider-image-2.jpeg";
 import Slider3 from "../../Assets/slider-image-3.jpeg";
+
+import Slider5 from "../../Assets/grocery-banner-2.jpeg"
+import Slider6 from "../../Assets/slider-2.jpeg"
 import Slider from "react-slick";
 import Blog1 from "../../Assets/blog-img-1.jpeg";
 import Blog2 from "../../Assets/blog-img-2.jpeg";
@@ -21,14 +24,16 @@ export default function MainSlider() {
       <div className="row p-5 ">
         <div className="col-md-10 container ">
           <Slider {...settings}>
-            <img height={400} src={Slider1} className="w-100" alt="slider" />
-            <img height={400} src={Slider2} className="w-100" alt="slider" />
-            <img height={400} src={Slider3} className="w-100" alt="slider" />
+            <img height={500} src={Slider1} className="w-100" alt="slider" />
+            <img height={500} src={Slider2} className="w-100" alt="slider" />
+            <img height={500} src={Slider3} className="w-100" alt="slider" />
+            <img height={500} src={Slider5} className="w-100" alt="slider" />
+            <img height={500} src={Slider6} className="w-100" alt="slider" />
           </Slider>
         </div>
-        <div className="col-md-2 ">
-          <img src={Blog1} height={200 } className="w-100" alt="blog" />
-          <img src={Blog2} height={200 } className="w-100" alt="blog" />
+        <div className="col-md-2">
+          <img src={Blog1} height={250 } className="w-100 pb-2" alt="blog" />
+          <img src={Blog2} height={250 } className="w-100 pt-2" alt="blog" />
         </div>
       </div>
     </main>
--- src/Components/WishList/WishList.jsx
+++ src/Components/WishList/WishList.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { WishList } from "../../Context/WishListContext";
 import { CartContext } from "../CartContext/CartContext";
+import toast from "react-hot-toast";
 
 export default function Wishlist() {
   const {
@@ -10,7 +11,124 @@ export default function Wishlist() {
   } = useContext(WishList);
 
   const [Rendering, setRendering] = useState([]);
+  const { AddToOCart } = useContext(CartContext);
+  const [Add, setAdd] = useState([]);
 
+  async function LoggedUserWishlist() {
+    try {
+      const { data } = await GetLoggedUserWishlist();
+      setRendering(data);
+      console.log(data);
+      setWishListNotifications(data?.data?.length);
+    } catch (error) {
+      console.error("ERR FROM WISHLIST PAGE " + error);
+    }
+  }
+
+  useEffect(() => {
+    LoggedUserWishlist();
+  }, []);
+
+  async function DeletingWishList(productId) {
+    try {
+      const { data } = await RemoveProductFromWishlist(productId);
+      // Fetch the updated wishlist data
+      const updatedWishlist = await GetLoggedUserWishlist();
+      setRendering(updatedWishlist.data); // Update the state with the updated data
+      console.log(updatedWishlist.data);
+      setWishListNotifications(data?.data?.length);
+      console.log(data);
+      toast.success("Product removed successfully From your wishlist");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function AddtoCartFromFeaturedDetails(productId) {
+    try {
+      const { data } = await AddToOCart(productId);
+      setAdd(data);
+      console.log("Item added to cart");
+      console.log(data);
+      toast.success(data.message);
+    } catch (error) {
+      console.error(error);
+      toast.error("something went wrong");
+    }
+  }
+
+  useEffect(() => {}, [AddtoCartFromFeaturedDetails]);
+
+  return (
+    <main className="pt-5">
+      <div className="container bg-main-light">
+        <h1 className="fw-bolder pt-5">WishList</h1>
+        {Rendering && (
+          <h3 className="fw-bolder pt-3 ">
+            Items
+            <span className="fw-bolder"> {Rendering.count}</span>
+          </h3>
+        )}
+
+        <div>
+          {Rendering?.data?.map((Item) => (
+            <div key={Item.id}>
+              <div className="d-flex justify-content-start align-items-center py-2">
+                <div className="w-25">
+                  <img
+                    className="w-100"
+                    src={Item.imageCover}
+                    alt={Item.title}
+                  />
+                </div>
+                <div className="w-75 px-5">
+                  <h2 className="fw-bolder text-main pt-5">
+                    {Item.title
+                      ? Item.title.split(" ").slice(0, 2).join(" ")
+                      : "Title Not Available"}
+                  </h2>
+                  <h2 className="fw-bolder text-main pt-5">{Item.price} EGP</h2>
+                  <div className="py-5 d-flex justify-content-start gap-5">
+                    <button
+                      onClick={() => AddtoCartFromFeaturedDetails(Item.id)}
+                      className="btn w-25 bg-primary fw-bolder text-white"
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
+                  <button
+                    onClick={() => DeletingWishList(Item.id)}
+                    className="btn w-25"
+                  >
+                    <li className=" text-danger fas fa-trash-can"> </li> Remove
+                    Item
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+/**
+ * ADD TO CART WORKS 
+ * import React, { useContext, useEffect, useState } from "react";
+import { WishList } from "../../Context/WishListContext";
+import { CartContext } from "../CartContext/CartContext";
+
+export default function Wishlist() {
+  const {
+    GetLoggedUserWishlist,
+    RemoveProductFromWishlist,
+    setWishListNotifications,
+  } = useContext(WishList);
+
+  const [Rendering, setRendering] = useState([]);
+  const { AddToOCart } = useContext(CartContext);
+  const [Add, setAdd] = useState([]);
   async function LoggedUserWishlist() {
     try {
       const { data } = await GetLoggedUserWishlist();
@@ -32,15 +150,15 @@ export default function Wishlist() {
   async function DeletingWishList(productId) {
     try {
       const { data } = await RemoveProductFromWishlist(productId);
-      window.location.reload();
+      //window.location.reload();
+      setRendering(data);
       console.log(data?.data?.length);
     } catch (error) {
       console.error(error);
     }
   }
   /////////////////////////Add to CART////////////////
-  const { AddToOCart } = useContext(CartContext);
-  const [Add, setAdd] = useState([]);
+
 
   async function AddtoCartFromFeaturedDetails(productId) {
     try {
@@ -105,4 +223,4 @@ export default function Wishlist() {
     </main>
   );
 }
-//
+**/
